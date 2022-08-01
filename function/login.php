1b8ae451f4b0dcfb8bfa43b7235a40a3ee75d874
François K <francoiska_fr@icloud.com>
2022-08-01 12:19:29 +0200

    debug1

--- function/login.php
+++ function/login.php
@@ -1,17 +1,28 @@
 <?php 
+echo 'ici ca va'; 
+
 require_once "../Controller/UserController.php";
 
+echo '1'; 
+
 $user = new UserController();
+
+echo '2'; 
+
+
 if (empty($_POST['emailLog']) || empty($_POST['password']))
 {
-
+    echo '3'; 
  echo 'Il y a eu une erreur';
 
 
 } else {
+    echo '4'; 
  $user->isConnected($_POST['emailLog'],$_POST['password']);
+ echo '5'; 
 
 header('Location: https://projet-my-movies-php.herokuapp.com ');
+echo '6'; 
 exit();
 
 echo "Personne n'est censé me voir"; 
