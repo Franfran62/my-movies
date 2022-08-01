30cd9efe521a3eb7d5a954dcb4aa09eddb7f22cc
François K <francoiska_fr@icloud.com>
2022-08-01 23:42:22 +0200

    Add Javascript for enable mobile use

--- view/inscription.php
+++ view/inscription.php
@@ -74,6 +74,10 @@ if ($_POST) {
 <script type="text/javascript">
         document.getElementById('myForm').submit();
     </script>
+
+   <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
