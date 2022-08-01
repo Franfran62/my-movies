6605be589b35172b48c81c0fa4fc3de5b95d969e
François K <francoiska_fr@icloud.com>
2022-08-01 12:02:16 +0200

    Redirection correction

--- html-partial/header.php
+++ html-partial/header.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="http://localhost:8888/my-movies/index.php">
+            <a class="navbar-brand" href="https://projet-my-movies-php.herokuapp.com">
                 <i class="bi bi-film"></i> My Movies </a>
             <button class="navbar-toggler" 
                     type="button" 
@@ -16,15 +16,15 @@
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="http://localhost:8888/my-movies/index.php">Home</a>
+                        <a class="nav-link" href="https://projet-my-movies-php.herokuapp.com">Home</a>
                     </li>
 
                    <?= !empty($_SESSION["username"]) ? "<li class='nav-item '>
-                   <a class='nav-link' href='http://localhost:8888/my-movies/view/publication.php'> Publier </a>
+                   <a class='nav-link' href=https://projet-my-movies-php.herokuapp.com/view/publication.php'> Publier </a>
                    </li>" : "" ?>
 
                     <?= !empty($_SESSION["username"]) ? "<li class='nav-item '>
-                   <a class='nav-link' href='http://localhost:8888/my-movies/view/bibliotheque.php'> Ma bibliothèque </a>
+                   <a class='nav-link' href='https://projet-my-movies-php.herokuapp.com/view/bibliotheque.php'> Ma bibliothèque </a>
                    </li>" : "" ?>
                 </ul>
 
@@ -33,14 +33,14 @@
                 <?= !empty($_SESSION["username"]) ? "<li class='nav-item nav-link active'> Bonjour {$_SESSION["username"]} !</li>" : "" ?>
                 <li class="nav-item">
                     <?=
-                     !empty($_SESSION) ? '<a class="nav-link active" href="http://localhost:8888/my-movies/function/disconnect.php"> Se déconnecter</a>' :
-                    '<a class="nav-link" href="http://localhost:8888/my-movies/view/connection.php">Se connecter</a>'
+                     !empty($_SESSION) ? '<a class="nav-link active" href="https://projet-my-movies-php.herokuapp.com/function/disconnect.php"> Se déconnecter</a>' :
+                    '<a class="nav-link" href="https://projet-my-movies-php.herokuapp.com/view/connection.php">Se connecter</a>'
                      ?>
                 </li>
                 <li class="nav-item">
                      <?=
                      !empty($_SESSION) ? '' :
-                    '<a class="nav-link" href="http://localhost:8888/my-movies/view/inscription.php">Inscription </a>'
+                    '<a class="nav-link" href="https://projet-my-movies-php.herokuapp.com/view/inscription.php">Inscription </a>'
                      ?>
                 </li>
 
